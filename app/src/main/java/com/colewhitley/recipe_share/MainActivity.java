fc60aa4075b021ff7aa8f0ff535f80a2d77a359d
Cole Whitley <colewhit29@gmail.com>
2017-10-29 15:11:29 -0400

    init commit

---
+++ app/src/main/java/com/colewhitley/recipe_share/MainActivity.java
@@ -0,0 +1,13 @@
+package com.colewhitley.recipe_share;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
