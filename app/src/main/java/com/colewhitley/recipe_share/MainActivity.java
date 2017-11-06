25be7a497fa405a38cdb97ee482951b77f8f50db
jvoves@sqad.com-at-202332648106 <jvoves@sqad.com>
2017-11-06 11:11:21 -0500

    xml layout setup

--- app/src/main/java/com/colewhitley/recipe_share/MainActivity.java
+++ app/src/main/java/com/colewhitley/recipe_share/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TabHost;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -20,14 +22,19 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
+    //constant for on actvity result. Tells us signInIntent called $finish
     private static final int RC_SIGN_IN = 1;
+
+    //current user
+    private FirebaseUser user;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    GoogleApiClient mGoogleApiClient;
     private GoogleSignInOptions gso;
 
-    GoogleApiClient mGoogleApiClient;
+    TabHost host; //tab container
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,48 +47,61 @@ public class LoginActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
 
+        //api client. GIves us access to google apis
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .enableAutoManage(this /* FragmentActivity */,null /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
-
+        //firebase auth instance
         mAuth = FirebaseAuth.getInstance();
+        //simple listener that checks if the user signs in/out while already in app
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    // User is signed in
                     Log.d("SignIn", "onAuthStateChanged:signed_in:" + user.getUid());
                 } else {
-                    // User is signed out
                     Log.d("SignIn", "onAuthStateChanged:signed_out");
                 }
-                // ...
             }
         };
 
+        host = (TabHost)findViewById(R.id.tabHost);
+        initTabs();
+        host.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String s) {
+                setTabStyle();
+            }
+        });
     }
 
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
+        user = mAuth.getCurrentUser();
+
+        //sign user in if they are not
+        if(user == null){ signIn(); }
+
+        Toast.makeText(MainActivity.this, "Welcome Back, " + user.getDisplayName(),
+                Toast.LENGTH_SHORT).show();
     }
+
 
     @Override
     public void onStop() {
         super.onStop();
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
-
         }
     }
 
     private void signIn() {
+        //googles sign in page
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
@@ -99,7 +119,42 @@ public class LoginActivity extends AppCompatActivity {
                 firebaseAuthWithGoogle(account);
             } else {
                 // Google Sign In failed, update UI appropriately
-                // ...
+            }
+        }
+    }
+
+    private void initTabs() {
+        host.setup();
+
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("My Recipes");
+        host.addTab(spec);
+
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("Create Recipe");
+        host.addTab(spec);
+
+        spec = host.newTabSpec("Tab Three");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("Find Recipes");
+        host.addTab(spec);
+
+        setTabStyle();
+    }
+
+    private void setTabStyle() {
+        for(int i=0;i<host.getTabWidget().getChildCount();i++)
+        {
+            TextView tv = (TextView) host.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+            tv.setTextColor(getResources().getColor(R.color.textColor));
+
+            if (!host.getTabWidget().getChildTabViewAt(i).equals(host.getCurrentTabView())){
+                host.getTabWidget().getChildTabViewAt(i).setAlpha(.5f);
+            }
+            else{
+                host.getTabWidget().getChildTabViewAt(i).setAlpha(1.0f);
             }
         }
     }
@@ -115,12 +170,12 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("AuthSuccess", "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
+                            user = mAuth.getCurrentUser();
                             //updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w("AuthFailed", "signInWithCredential:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                             //updateUI(null);
                         }
