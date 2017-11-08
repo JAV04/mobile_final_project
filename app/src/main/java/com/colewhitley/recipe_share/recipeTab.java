aa3afca098a9242d9c36c96e6a865d3583647790
jvoves@sqad.com-at-202332648106 <jvoves@sqad.com>
2017-11-08 11:08:01 -0500

    tabs now activities

---
+++ app/src/main/java/com/colewhitley/recipe_share/recipeTab.java
@@ -0,0 +1,13 @@
+package com.colewhitley.recipe_share;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class recipeTab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_tab);
+    }
+}
